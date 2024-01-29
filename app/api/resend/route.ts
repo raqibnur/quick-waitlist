8c1e7de814a49ad12e65624f9954f266ff87ff0e
Rakibce <rakibce30@gmail.com>
2024-01-29 22:38:37 +0600

    create contacts in audienced

--- app/api/resend/route.ts
+++ app/api/resend/route.ts
@@ -4,19 +4,29 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
 const fromEmail = process.env.NEXT_PUBLIC_FROM_EMAIL;
 const siteName = process.env.NEXT_PUBLIC_SITE_NAME;
+const audienceId = process.env.NEXT_PUBLIC_AUDIENCE_ID;
 
 export async function POST(req: Response) {
   const body = await req.json();
-  console.log(body.email, "api");
+
   try {
-    const data = await resend.emails.send({
+    const sendEmail = await resend.emails.send({
       from: fromEmail as string,
       to: [body.email],
       subject: siteName as string,
       react: EmailTemplate(),
     });
 
-    return Response.json(data);
+    const addContact = await resend.contacts.create({
+      email: body.email,
+      unsubscribed: false,
+      audienceId: audienceId as string,
+    });
+
+    return Response.json({
+      sendEmail,
+      addContact,
+    });
   } catch (error) {
     return Response.json({ error });
   }
